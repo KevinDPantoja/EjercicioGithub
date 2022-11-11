6e4071d160d1dba6e112c1c9bda8d2a29ee5e026
Jhonatan Acuña <jhonatanacuna@cbit-online.com>
2022-11-11 11:15:33 -0500

    Se documento el controller

--- src/main/java/org/example/animals/carpincho/CarpinchoController.java
+++ src/main/java/org/example/animals/carpincho/CarpinchoController.java
@@ -3,11 +3,21 @@ package org.example.animals.carpincho;
 public class CarpinchoController implements CarpinchoInterface {
     private CarpinchoModel carpincho;
 
+    /**
+     * Se crea un objeto de la clase CarpinchoModel para poder crear un carpincho
+     *
+     * @param nombre
+     * @param peso
+     * @param altura
+     */
     @Override
     public void crearCarpincho(String nombre, int peso, double altura) {
         carpincho = new CarpinchoModel(nombre, peso, altura);
     }
 
+    /**
+     * Muestra los atributos del carpincho por pantalla
+     */
     @Override
     public void mostrarCarpincho() {
         System.out.println("Nombre: " + carpincho.getNombre());
