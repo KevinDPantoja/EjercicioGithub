84eb968d1c84581b76f264377251f50d1cddbd78
Jhonatan Acuña <jhonatanacuna@cbit-online.com>
2022-11-11 11:11:09 -0500

    Se implmenta el controller en el main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import org.example.animals.carpincho.CarpinchoController;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        // NO BORRAR Crear un objeto de la clase CarpinchoController
+        CarpinchoController carpincho = new CarpinchoController();
+        carpincho.crearCarpincho("Carpincho", 54, 1.2);
+        carpincho.mostrarCarpincho();
+
     }
 }
