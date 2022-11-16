59fb464cc1529cbb3459392b958a6d7e7eaef7c9
Kevin Pantoja <kevinpantoja@cbit-online.com>
2022-11-16 08:57:53 -0500

    Se agrego la clase tigre

--- src/main/java/org/example/animals/tigre/tigre.java
+++ src/main/java/org/example/animals/tigre/tigre.java
@@ -1,4 +1,45 @@
 package org.example.animals.tigre;
-
 public class tigre {
+    private String color;
+    private String altura;
+    private String peso;
+
+    public tigre(String color, String altura, String peso) {
+        this.color = color;
+        this.altura = altura;
+        this.peso = peso;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getAltura() {
+        return altura;
+    }
+
+    public void setAltura(String altura) {
+        this.altura = altura;
+    }
+
+    public String getPeso() {
+        return peso;
+    }
+
+    public void setPeso(String peso) {
+        this.peso = peso;
+    }
+
+    @Override
+    public String toString() {
+        return "Tigre{" +
+                "color='" + color + '\'' +
+                ", altura='" + altura + '\'' +
+                ", peso='" + peso + '\'' +
+                '}';
+    }
 }
