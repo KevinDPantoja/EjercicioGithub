b34bc8ec2bec44ed313273eca329fef618ff1374
Juan.D.Vasquez <juanvasquez@cbit-online.com>
2022-11-16 10:18:06 -0500

    se agregan comentarios al codigo

--- src/main/java/org/example/animals/pangolin/Pangolin.java
+++ src/main/java/org/example/animals/pangolin/Pangolin.java
@@ -1,16 +1,17 @@
 package org.example.animals.pangolin;
 
 public class Pangolin {
+    //se crean las variables de nombre peso y altura del pangolin
     private String nombre;
     private int peso;
     private int altura;
-
+    //se crea el constructor del pangolin
     public Pangolin(String nombre, int peso, int altura) {
         this.nombre = nombre;
         this.peso = peso;
         this.altura = altura;
     }
-
+    //se crean los getter y setter del pangolin
     public String getNombre() {
         return nombre;
     }
